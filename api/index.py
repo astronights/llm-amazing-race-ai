52b13d3b336847a95f617ffd1cdd16a6dc80fc22
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-08-31 10:49:03 +0800

    Updating CORS

--- api/index.py
+++ api/index.py
@@ -1,11 +1,30 @@
-from .services.web_utils import get_all_cities, get_coordinates, get_attractions
-from .services.llm import chat
+import os
 
-from flask import Flask, request
 from flask_cors import CORS
+from flask import Flask, request, jsonify
+
+from .services.llm import chat
+from .services.web_utils import get_all_cities, get_coordinates, get_attractions
+
+
+def get_allowed_origins():
+    environment = os.getenv('FLASK_ENV', 'development')
+    if environment == 'production':
+        return ["https://llm-amazing-race.vercel.app"]
+    elif environment == 'development':
+        return ["http://localhost:3000"]
+    return []  # No origins allowed by default
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000", "https://https://llm-amazing-race.vercel.app/"])
+CORS(app, resources={r"/*": {"origins": get_allowed_origins()}})
+
+@app.before_request
+def restrict_cors():
+    if 'FLASK_ENV' in os.environ and os.environ['FLASK_ENV'] == 'production':
+        origin = request.headers.get('Origin')
+        if origin not in get_allowed_origins():
+            return jsonify({"error": "Forbidden"}), 403
+
 
 @app.route('/')
 def root_hello():
